c21a612befeeb2e323e3a63d11443242bb89449e
shreyasrivastava11 <112487189+shreyasrivastava11@users.noreply.github.com>
2024-09-04 11:46:34 +0530

    push

--- MegaBlog/src/appwrite/auth.js
+++ MegaBlog/src/appwrite/auth.js
@@ -3,13 +3,19 @@ import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
   client = new Client();
+  // CLient is declared to build the connection of appwrite with the application
 
   constructor() {
     this.client
       .setEndpoint(conf.appwriteUrl)
       .setProject(conf.appwriteProjectId);
 
+    // set EndPoint defines how the application connect to the appwrite server
+    // set Project is used to give the interaction of application with the specific appwrite project
+
     this.account = new Account(this.client);
+
+    // Through account declaration appwrite provide some sought of method by which we can build the functions for account
   }
 
   async createAccount({ email, password, name }) {
